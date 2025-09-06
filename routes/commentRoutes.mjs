de12533c4bed0f28009f55fc26fc48576890918c
seraj <serajstudy1@gmail.com>
2025-09-06 13:47:08 -0400

    Comments changes mistake and corrected

--- routes/commentRoutes.mjs
+++ routes/commentRoutes.mjs
@@ -18,20 +18,20 @@ router
     // Check if we have all data needed to create user
     if (name && username && email) {
       // check is username exists!!
-      if (comments.find((u) => u.username == username)) {
+      if (comments.find((comment) => comment.username == username)) {
         res.status(400).json({ err: "Commentsname taken" });
         return;
       }
 
-      const user = {
+      const comment = {
         id: comments[comments.length - 1].id + 1, //find the last comments id number and add one to it.
         name,
         username,
         email,
       };
-      comments.push(user);
-      res.json(user);
-    } else res.status(400).json({ msg: "Insuffecient Data" });
+      comments.push(comment);
+      res.json(comment);
+    } else res.status(400).json({ msg: "Insufficient Data" });
   });
 
 //  @route GET /api/comments/:id
@@ -40,18 +40,18 @@ router
 router
   .route("/:id")
   .get((req, res, next) => {
-    const user = comments.find((user) => user.id == req.params.id);
+    const comment = comments.find((comment) => comment.id == req.params.id);
 
-    if (user) res.json(user);
+    if (comment) res.json(comment);
     else next();
   })
   .patch((req, res, next) => {
-    // find the user that the client wants to change
+    // find the comments that the client wants to change
     const id = req.params.id;
     const data = req.body;
 
-    const user = comments.find((user, i) => {
-      if (user.id == id) {
+    const comment = comments.find((comment, i) => {
+      if (comment.id == id) {
         for (const item in data) {
           comments[i][item] = data[item]; // make the changes
         }
@@ -60,22 +60,22 @@ router
     });
 
     // send a response back to the client
-    if (user) {
+    if (comment) {
       res.json(comments);
     } else next();
   })
   .delete((req, res, next) => {
     // find user the client wants to delete
     const id = req.params.id;
-    const user = comments.find((user, i) => {
-      if (user.id == id) {
+    const comment = comments.find((comment, i) => {
+      if (comment.id == id) {
         comments.splice(i, 1); // remove the user at index i
         return true;
       }
     });
 
     // send a response back to the client
-    if (user) {
+    if (comment) {
       res.json(comments);
     } else next();
   });
